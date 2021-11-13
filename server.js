1c1a45d0c2f612f7f30f10f76ddde54c34200a14
AdamPiatkowski <picus1991@gmail.com>
2021-11-13 16:32:52 +0100

    dodanie zmian na nowej galezi

--- server.js
+++ server.js
@@ -1,11 +1,10 @@
 const app = express();
 
 //dodanie zmian
-<<<<<<< HEAD
 
 
 
 //zmiany w pliku do rebase
-=======
 //dodanie kolejnych zmian
->>>>>>> 2eb50b8e9da544cbc3f65d3a927ed8fff3a5b0bc
+
+//zmiany na nowej galezi
